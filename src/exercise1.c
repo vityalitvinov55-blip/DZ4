9f7cc0134de3082b981fd58003559283608aa6bc
Vitya Lit <vita.litvionov.55@gmail.com>
2025-10-15 10:27:26 +0700

    Решонные задания

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,17 +2,16 @@
 
 int main(int argc, char** argv)
 {
-    float numbers[10] = {};
-    scanf("%f %f %f %f %f %f %f %f %f %f", &numbers[0], &numbers[1], &numbers[2],
-          &numbers[3], &numbers[4], &numbers[5], &numbers[6], &numbers[7],
-          &numbers[8], &numbers[9]);
-
-    float middle = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        middle += numbers[i];
+    float a[10] = {};
+    for (int i = 0; i < 10; i++) {
+        scanf("%lf", &a[i]);
     }
 
-    middle /= 10;
-    printf("%f\n", middle);
+    float c = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        c += a[i];
+    }
+
+    printf("%f\n", c/10);
 }
