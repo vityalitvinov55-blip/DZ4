0c30a5f3900d8c600b866c9d4a156d3e9b12adf5
vityalitvinov55-blip <vita.litvinov.55@gmail.com>
2025-10-29 16:44:08 +0700

    Update exercise3.c

--- src/exercise3.c
+++ src/exercise3.c
@@ -2,13 +2,13 @@
 #include<stdlib.h>
 
 int main(int argc, char* argv[]){
-    int a[10];
-    int k = atoi(argv[11]);
+    double a[10];
+    int k;
 
     for(int i = 0; i<10; i++){
-        a[i] = atoi(argv[i+1]);
+        scanf("%lf", &a[i]);
     }
-
+    scanf("%lf",&k);
     for(int i =0; i < 10; i++){
         printf("%d ", a[(i-k + 10)%10]);
     }
